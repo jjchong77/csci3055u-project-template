f4d80bfa362fe4c237fc167882a178f27a74e408
Jeremy Chong <jjchong77@gmail.com>
2018-12-11 04:24:24 -0500

    Add files via upload

--- basic-syntax/loops.rs
+++ basic-syntax/loops.rs
@@ -8,4 +8,16 @@ fn main ()
         if (num==10)
         {break;}
     }
+    let myar = [1, 2, 3, 4, 5];
+
+    for x in 0..10 //for loop
+    {
+    println!("Iteration no {}", x); // x: i32
+    }
+    println!("Second for loop");
+    for x in 0..myar.len()
+    {
+        println!("Iteration no {}", myar[x]); // prints the elements in the array
+
+    }
 }
