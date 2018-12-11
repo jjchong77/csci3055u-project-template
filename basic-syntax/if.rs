312fe18666deb7e0267ab5a39135b64787617a8c
Jeremy Chong <jjchong77@gmail.com>
2018-12-11 01:29:40 -0500

    Add files via upload

---
+++ basic-syntax/if.rs
@@ -0,0 +1,17 @@
+fn main ()
+{
+let num = 0.0;
+
+    if num<0.0
+    {
+        println!("negative");
+    }
+    else if num > 0.0
+    {
+        println!("positive");
+    }
+    else
+    {
+        println!("zero");
+    }
+}
---
+++ basic-syntax/loops.rs
@@ -0,0 +1,11 @@
+fn main ()
+{
+   let mut num=0;
+    loop
+    {
+        num=num+1;
+        println!("Iteration no {}", num);
+        if (num==10)
+        {break;}
+    }
+}
